5789ed19acc27eea80402ab9ac87e4c03d2ce66a
HerlyRyan <herlyryanth@gmail.com>
2026-06-23 14:37:40 +0800

    Modified: add validate middleware

--- src/services/notes/routes/index.js
+++ src/services/notes/routes/index.js
@@ -1,12 +1,14 @@
 import e from 'express';
 import { createNote, deleteNoteById, editNoteById, getNoteById, getNotes } from '../controller.js';
+import validate from '../../../middlewares/validate.js';
+import { notePayloadSchema } from '../validator/schema.js';
 
 const router = e.Router();
 
 router.get('/notes', getNotes);
 router.get('/notes/:id', getNoteById);
-router.post('/notes', createNote);
-router.put('/notes/:id', editNoteById);
+router.post('/notes', validate(notePayloadSchema), createNote);
+router.put('/notes/:id', validate(notePayloadSchema), editNoteById);
 router.delete('/notes/:id', deleteNoteById);
 
 export default router;
