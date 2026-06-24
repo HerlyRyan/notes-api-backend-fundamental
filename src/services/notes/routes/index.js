df66b6331423bdd79ae26c605f60b79b2f727ca7
HerlyRyan <herlyryanth@gmail.com>
2026-06-24 12:19:42 +0800

    Modified: add validateQuery middleware to index.js routes notes, and adding note query schema

--- src/services/notes/routes/index.js
+++ src/services/notes/routes/index.js
@@ -1,11 +1,11 @@
 import e from 'express';
 import { createNote, deleteNoteById, editNoteById, getNoteById, getNotes } from '../controller.js';
-import validate from '../../../middlewares/validate.js';
-import { notePayloadSchema } from '../validator/schema.js';
+import { validate, validateQuery } from '../../../middlewares/validate.js';
+import { notePayloadSchema, noteQuerySchema } from '../validator/schema.js';
 
 const router = e.Router();
 
-router.get('/notes', getNotes);
+router.get('/notes', validateQuery(noteQuerySchema), getNotes);
 router.get('/notes/:id', getNoteById);
 router.post('/notes', validate(notePayloadSchema), createNote);
 router.put('/notes/:id', validate(notePayloadSchema), editNoteById);
