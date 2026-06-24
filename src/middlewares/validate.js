6147ec11fda35c7880059a2baf03110fc5000725
HerlyRyan <herlyryanth@gmail.com>
2026-06-24 12:17:08 +0800

    Modified: add validation query middleware

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -10,4 +10,16 @@ const validate = (schema) => (req, res, next) => {
   next();
 };
 
-export default validate;
+const validateQuery = (schema) => (req, res, next) => {
+  const { error, value } = schema.validate(req.query, {
+    abortEarly: false,
+    allowUnknown: false,
+    stripUnknown: true,
+  });
+
+  if (error) return next(error);
+  req.validated = value;
+  next();
+};
+
+export { validate, validateQuery };
